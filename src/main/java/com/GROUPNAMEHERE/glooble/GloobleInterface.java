a845efbf6eb2411579fb0a770dd6bb19be2d00ff
William Faircloth <wfaircloth3@hawkmail.hccfl.edu>
2019-02-14 09:49:22 -0500

    included javax.swing.* and expanded menu bar

--- src/main/java/com/GROUPNAMEHERE/glooble/GloobleInterface.java
+++ src/main/java/com/GROUPNAMEHERE/glooble/GloobleInterface.java
@@ -11,10 +11,12 @@ package com.GROUPNAMEHERE.glooble;
  */
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*; 
 
 public class GloobleInterface extends Frame
 {   
     private Frame SearchWindow;
+    private Frame AdminWindow;
     public String SearchInput;
 
     public GloobleInterface()
@@ -27,8 +29,8 @@ public class GloobleInterface extends Frame
     //creating the user interface window with closable button here
     public void createSearchWindow()
     {
-        
-        SearchWindow = new Frame ("Glooble Search");
+        JFrame SearchWindow = new JFrame ("Glooble Search");
+        SearchWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         SearchWindow.setVisible(true);
         SearchWindow.setSize(600,600);
         SearchWindow.setBackground(Color.darkGray);
@@ -38,53 +40,97 @@ public class GloobleInterface extends Frame
             {
                 System.exit(0);
             }
-        });    
+        });  
+        
+        SearchWindow.setJMenuBar(createMenuBar());
+    }
+    
+    public void createAdminWindow()
+    {
+        JFrame AdminWindow = new JFrame ("Glooble Admin");
+        AdminWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        AdminWindow.setVisible(true);
+        AdminWindow.setSize(600,600);
+        AdminWindow.setBackground(Color.darkGray);
+        AdminWindow.addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        }); 
     }
     
     //editing window to include text and search bar
     public void createwindowItems()
     {
-      setLayout(new GridLayout(3,1));
-      
-      Label GloobleLabel;
-      GloobleLabel = new Label("Glooble Search", Label.CENTER);
-      GloobleLabel.setBackground(Color.darkGray);
-      GloobleLabel.setForeground(Color.blue);
-      GloobleLabel.setFont(new Font("ComicSansMS", Font.BOLD,20));
-      SearchWindow.add(GloobleLabel);
       
     }
 
-    //creating menu bar with file and settings menus
-    //here I primarily wanted to make a clickable link to the administrator UI
-    public void createMenuBar()
+    //here I primarily wanted to make the menu and a clickable link to the admin UI
+    public JMenuBar createMenuBar()
     {
-        final MenuBar GloobleMenu = new MenuBar();
-    
-        Menu fileMenu = new Menu("File");
-        Menu settingsMenu = new Menu("settings");
-        MenuItem AdminWindowItem = new MenuItem("administration");
-        AdminWindowItem.setActionCommand("administration");
-    
-        SearchWindow.setMenuBar(GloobleMenu);
-        SearchWindow.setVisible(true);
-    
-        GloobleMenu.add(fileMenu);
-        GloobleMenu.add(settingsMenu);
-    
-        settingsMenu.add(AdminWindowItem);
-    
-        AdminWindowItem.addActionListener((ActionEvent e) -> {
-            SearchWindow = new Frame ("Admin Window");
-            SearchWindow.setVisible(true);
-            SearchWindow.setSize(600,600);
-            SearchWindow.addWindowListener(new WindowAdapter()
+        JMenuBar GloobleBar;
+        JMenu menu, submenu;
+        JMenuItem menuSelect;
+        
+        GloobleBar = new JMenuBar();
+        
+        menu = new JMenu("File");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+        "File");
+        GloobleBar.add(menu);
+        
+        menuSelect = new JMenuItem("Open      Ctrl+O",KeyEvent.VK_T);
+        menu.add(menuSelect);
+        
+        menuSelect = new JMenuItem("Close",KeyEvent.VK_T);
+        menu.add(menuSelect);
+        
+        menuSelect = new JMenuItem("Save      Ctrl+S",KeyEvent.VK_T);
+        menu.add(menuSelect);
+        
+        menuSelect = new JMenuItem("SaveAs...",KeyEvent.VK_T);
+        menu.add(menuSelect);
+        
+        menu.addSeparator();
+        
+        menuSelect = new JMenuItem("Exit",KeyEvent.VK_T);
+        menu.add(menuSelect);
+        
+        menu = new JMenu("Settings");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+        "Settings");
+        GloobleBar.add(menu);
+        
+        menuSelect = new JMenuItem("General Settings",KeyEvent.VK_T);
+        menu.add(menuSelect);
+        
+        menu = new JMenu("Admin");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+        "Admin");
+        GloobleBar.add(menu);
+        
+        menuSelect = new JMenuItem("Open Admin window",KeyEvent.VK_T);
+        menuSelect.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
             {
-                public void windowClosing(WindowEvent e)
-                {
-                    System.exit(0);
-                }
-            });
+              createAdminWindow();   
+            }
         });
+       
+        menu.add(menuSelect);
+        
+        menu = new JMenu("About");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+        "About");
+        GloobleBar.add(menu);
+        
+        return GloobleBar;
     }
 }
